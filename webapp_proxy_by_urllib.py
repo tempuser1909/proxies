0a458529350f98325f46e97b9d12513bdce8131e
root <root@localhost.localdomain>
2016-09-26 06:45:12 -0400

    added more refined? proxy by urllib, dstill doesn't work properly because raw_input() does not stop and wait, resulting in print data continuously printing and the browser treat the returned string as a text file and not a html page

---
+++ webapp_proxy_by_urllib.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+import urllib2
+
+# open random socket to connect to web server (random, everytime you run this program)
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+http_method = ""
+data = ""
+data_field = ""
+user_input = ""
+filtered_user_input = ""
+input = ""
+while(1):
+	#print "test test"
+	# assume that this is a web application proxy
+	# get user input
+	# remember '\r\n\r\n'
+	
+	user_input = raw_input()
+	
+	
+	filtered_user_input = user_input #no filtering yet
+	
+	
+	fields = filtered_user_input.split('\r\n')
+	http_method = fields[0]
+	http_method_split = http_method.split(' ')
+	if(http_method_split[0] == 'GET'):
+		req = urllib2.Request('http://10.0.2.15'+http_method_split[1])
+	elif(http_method_split[0] == 'POST'):
+		req = urllib2.Request('http://10.0.2.15')
+	# obtain request via raw request
+	fields_without_method = fields[1:] #everything except first field
+	if(http_method_split[0] == 'POST'):
+		data_field = fields[len(fields)-3] #only last field
+	output = {}
+	for field in fields_without_method:
+		if(field == '' or ':' not in field):
+			break
+		key, value = field.split(':')
+		req.add_header(key, value)
+		
+	if(http_method_split[0] == 'POST'):
+		req.add_data(data_field)
+
+
+	# send it to 10.0.2.15:80
+	c_recv = urllib2.urlopen(req)
+	# wait for webserver to reply
+	data = c_recv.read()
+	print data
+	# loop until connection is terminated
+	
+	
+	
+	#print "end test"
